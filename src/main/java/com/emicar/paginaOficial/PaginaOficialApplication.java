6e7870f88a132b1bc3dc65fe55bfc51bbf5b0691
Mathias Fernandez <mathias_fernandez_24@hotmail.com>
2022-11-17 16:51:30 -0300

    inicio

---
+++ src/main/java/com/emicar/paginaOficial/PaginaOficialApplication.java
@@ -0,0 +1,13 @@
+package com.emicar.paginaOficial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaOficialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaOficialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emicar/paginaOficial/PaginaOficialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emicar.paginaOficial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaOficialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
